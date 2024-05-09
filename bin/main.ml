1f17a72eff2f9b1bdbd595c50f17415a8ee5c28e
Kasper <kasper.mustad.juell@linux.com>
2024-05-09 20:44:21 +0200

    Rename to HTcaML not HTcaML2

--- bin/main.ml
+++ bin/main.ml
@@ -2,12 +2,12 @@
 
 let () = print_endline "Hello, World!"
 
-let lexer = HTcaML2.Lexer.init "# *Test* \n## Hei Paa deg\n\nTesting\n- Liste\n\t- liste```lua\nlocal l = 1```";;
+let lexer = HTcaML.Lexer.init "# *Test* \n## Hei Paa deg\n\nTesting\n- Liste\n\t- liste```lua\nlocal l = 1```";;
 
-Fmt.pr "\n%s\n" (HTcaML2.Lexer.show lexer)
+Fmt.pr "\n%s\n" (HTcaML.Lexer.show lexer)
 
 let rec main lexer =
-    let lexer, t = HTcaML2.Lexer.next_token lexer in
+    let lexer, t = HTcaML.Lexer.next_token lexer in
 
     match t with
     | None -> Fmt.pr "None :/ \n"
@@ -23,14 +23,14 @@ let rec main lexer =
             | Star i    -> Fmt.pr "Star: %d\n" i
             | Illegal   -> Fmt.pr "Illegal\n"
         in
-        (* let () = Fmt.pr "%s\n" (HTcaML2.Lexer.show lexer) in *)
+        (* let () = Fmt.pr "%s\n" (HTcaML.Lexer.show lexer) in *)
         main lexer
 ;;
 main lexer;;
 
 let get_tokens builder =
     let rec loop builder l = 
-        let next = HTcaML2.Tree.next_node builder in
+        let next = HTcaML.Tree.next_node builder in
         match next with
         | _, None -> List.rev l
         | lexer, Some token ->
@@ -41,19 +41,19 @@ let get_tokens builder =
 let tree nodes =
     List.iter nodes ~f:(fun n ->
         match n with
-        | HTcaML2.Node.Next -> Fmt.pr "Next\n"
-        | Header1 h -> Fmt.pr "Header1: %s\n" (HTcaML2.Node.show_paragraph h)
-        | Header2 h -> Fmt.pr "Header2: %s\n" (HTcaML2.Node.show_paragraph h)
-        | Header3 h -> Fmt.pr "Header3: %s\n" (HTcaML2.Node.show_paragraph h)
-        | Header4 h -> Fmt.pr "Header4: %s\n" (HTcaML2.Node.show_paragraph h)
-        | Header5 h -> Fmt.pr "Header5: %s\n" (HTcaML2.Node.show_paragraph h)
-        | Header6 h -> Fmt.pr "Header6: %s\n" (HTcaML2.Node.show_paragraph h)
-        | Paragraph p -> Fmt.pr "Paragraph: %s\n" (HTcaML2.Node.show_pbody p)
-        | ListBody b -> Fmt.pr "List: %s\n" (HTcaML2.Node.show_list b)
+        | HTcaML.Node.Next -> Fmt.pr "Next\n"
+        | Header1 h -> Fmt.pr "Header1: %s\n" (HTcaML.Node.show_paragraph h)
+        | Header2 h -> Fmt.pr "Header2: %s\n" (HTcaML.Node.show_paragraph h)
+        | Header3 h -> Fmt.pr "Header3: %s\n" (HTcaML.Node.show_paragraph h)
+        | Header4 h -> Fmt.pr "Header4: %s\n" (HTcaML.Node.show_paragraph h)
+        | Header5 h -> Fmt.pr "Header5: %s\n" (HTcaML.Node.show_paragraph h)
+        | Header6 h -> Fmt.pr "Header6: %s\n" (HTcaML.Node.show_paragraph h)
+        | Paragraph p -> Fmt.pr "Paragraph: %s\n" (HTcaML.Node.show_pbody p)
+        | ListBody b -> Fmt.pr "List: %s\n" (HTcaML.Node.show_list b)
         | _ -> Fmt.pr "IDK\n")
 ;;
 
-let builder = HTcaML2.Tree.init_builder
+let builder = HTcaML.Tree.init_builder
 "Tekst
 **bold** *italic*
 
